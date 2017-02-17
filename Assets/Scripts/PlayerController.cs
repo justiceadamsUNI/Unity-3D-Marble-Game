e49c43f1c0fc25d4b7f07e22be6811b1707abce5
Ben Fick <fickb@uni.edu>
2017-02-16 23:51:22 -0600

    Added control to sphere and camera movement
    Added two scripts, one for the sphere and one for the camera. Control
    sphere through arrow keys on computer

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb; 
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate(){
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (horizontal, 0.0f, vertical);
+
+		//rb.AddTorque (movement * speed);
+		rb.AddForce (movement * speed);
+	}
+		
+}
