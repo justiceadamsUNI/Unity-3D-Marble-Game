44e821d238b706964dfbced9fb3611d57ef4aad4
justiceadamsUNI <adamsjal@uni.edu>
2017-03-01 19:27:35 -0600

    respawn mechanism complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
-	private Rigidbody rb; 
+	private Rigidbody rb;
+    private Vector3 intialPosition;
 
 	void Start(){
 		rb = GetComponent<Rigidbody> ();
-	}
+        intialPosition = transform.position;
+    }
 
 	void FixedUpdate(){
 		float horizontal = Input.GetAxis ("Horizontal");
@@ -19,5 +21,21 @@ public class PlayerController : MonoBehaviour {
 		//rb.AddTorque (movement * speed);
 		rb.AddForce (movement * speed);
 	}
-		
+
+    void OnTriggerEnter(Collider other) {
+        switch(other.tag) {
+            //Happens when a player falls of the map.
+            case "Fall Detector":
+                respawn();
+                break;
+        }
+    }
+
+    private void respawn() {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        rb.AddForce(Vector3.zero);
+
+        transform.position = intialPosition;
+    }
 }
