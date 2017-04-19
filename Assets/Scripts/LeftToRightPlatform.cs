2f152d55e928427a08fa14b84d4868245e1a5d43
justiceadamsUNI <adamsjal@uni.edu>
2017-04-18 21:08:31 -0500

    Added new moving platforms and have begun initial level design

---
+++ Assets/Scripts/LeftToRightPlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftToRightPlatform : MonoBehaviour {
+    private float initialX;
+    public float speed;
+    public float travelDistance;
+
+
+    // Use this for initialization
+    void Start () {
+        initialX = transform.position.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(
+            initialX - (travelDistance / 2) + Mathf.PingPong(Time.time * speed, 10),
+            transform.position.y,
+            transform.position.z);
+
+    }
+}
